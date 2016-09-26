a46a7b9a65b94085d7b504ca19c1c29e533c5640
takuo hada <t_hada@dachicraft.net>
2016-09-26 20:19:28 +0900

    整理元のヘッダファイル削除

--- src/include/magica/int.h
+++
@@ -1,9 +0,0 @@
-#pragma once 
-
-#include <magica/int/uint32.h>
-#include <magica/int/uint64.h>
-#include <magica/int/uint128.h>
-#include <magica/int/uint256.h>
-
-#include <magica/int/uint128_impl.h>
-#include <magica/int/uint256_impl.h>
